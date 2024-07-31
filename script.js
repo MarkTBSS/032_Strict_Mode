5f364aa7afad4d345734c56423380f28e18e611b
MarkTBSS <marktbss@gmail.com>
2024-07-31 08:04:49 +0700

    [ADD] First Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// "use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive :D");
+
+const interface = "Audio"; // Uncaught SyntaxError: Unexpected token 'interface'
+const private = 534; // Uncaught SyntaxError: Unexpected token 'private'
